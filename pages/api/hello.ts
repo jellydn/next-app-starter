55f8b12faa2589691bc41e3a14836481f76e5b0b
Huynh Duc Dung <dung@aircarbon.co>
2021-04-21 18:53:17 +0800

    chore(api): add typing

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
 
-export default (req, res) => {
+export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: "John Doe" });
 };
