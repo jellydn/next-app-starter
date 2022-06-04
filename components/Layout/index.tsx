41de9c097fc2fa2ae8ae5c733ae48935e394781b
Huynh Duc Dung <dunghd.it@gmail.com>
2022-06-05 01:13:39 +0800

    :sparkles: add google adsense

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -1,7 +1,16 @@
+import Head from 'next/head';
+
 import Header from '../Header';
 
 const Layout: React.FC<React.PropsWithChildren<unknown>> = ({ children }) => (
     <div>
+        <Head>
+            <script
+                async
+                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5337133458846513"
+                crossOrigin="anonymous"
+            ></script>
+        </Head>
         <Header
             links={[
                 {
