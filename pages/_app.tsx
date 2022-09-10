b63de7292f0b4669668e6019c5f3742f2b7b3f02
Huynh Duc Dung <dunghd.it@gmail.com>
2022-09-10 17:01:34 +0800

    :arrow_up: update deps

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,7 +21,7 @@ if (process.env.NEXT_PUBLIC_API_MOCKING === 'yes') {
 
 const queryClient = new QueryClient();
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<any>) {
     const { initialState } = pageProps;
     return (
         <Provider
