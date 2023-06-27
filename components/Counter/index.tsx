1be18ad7d80cfd22d00cb453aa1da1ef62eb96ec
Salman Sheriff <95226945+salmansheri@users.noreply.github.com>
2023-06-27 19:48:59 +0530

    styled button (#1247)

--- components/Counter/index.tsx
+++ components/Counter/index.tsx
@@ -6,7 +6,7 @@ import Button from "../Button";
 function Counter() {
   const [count, setCount] = useAtom(counterAtom);
   return (
-    <div className="flex flex-col justify-center mb-8">
+    <div className="flex flex-col justify-center my-5">
       <Button
         onClick={() => setCount((counter) => counter + 1)}
         size="s"
