ad3ac13ff78f302eb7d37a132fcf20df990da125
opacteam <mike@minisisinc.com>
2023-10-16 18:35:50 -0700

    add: google & github providers

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,6 +3,9 @@ import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import * as Prisma from '@prisma/client';
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
+import GithubProvider from "next-auth/providers/github"
+import GoogleProvider from "next-auth/providers/google";
+
 
 const prisma = new Prisma.PrismaClient();
 export default NextAuth({
@@ -11,6 +14,14 @@ export default NextAuth({
             server: process.env.EMAIL_SERVER,
             from: process.env.EMAIL_FROM,
         }),
+         GithubProvider({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET,
+        }),
+          GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET
+  })
     ],
     adapter: PrismaAdapter(prisma),
 });
