4a32146f302dda77c32ee6eebc43b46533ffe862
mikah13 <anhminhhoang13@gmail.com>
2023-10-18 04:49:02 -0700

    add: condition check for next-auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,27 +1,44 @@
 /* eslint-disable new-cap */
-import { PrismaAdapter } from '@next-auth/prisma-adapter';1
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import * as Prisma from '@prisma/client';
 import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
-import GithubProvider from "next-auth/providers/github"
-import GoogleProvider from "next-auth/providers/google";
-
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
 
 const prisma = new Prisma.PrismaClient();
+
+
+// Default option
+const providers = [
+  EmailProvider({
+    server: process.env.EMAIL_SERVER,
+    from: process.env.EMAIL_FROM,
+  }),
+];
+
+// GitHub Provider
+if (process.env.GITHUB_ID && process.env.GITHUB_SECRET) {
+  providers.push(
+    GithubProvider({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    })
+  );
+}
+
+// Google Provider
+
+if (process.env.GOOGLE_CLIENT_ID && process.env.GOOGLE_CLIENT_SECRET) {
+  providers.push(
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    })
+  );
+}
+
 export default NextAuth({
-    providers: [
-        EmailProvider({
-            server: process.env.EMAIL_SERVER,
-            from: process.env.EMAIL_FROM,
-        }),
-         GithubProvider({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET,
-        }),
-          GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-  })
-    ],
-    adapter: PrismaAdapter(prisma),
+  providers,
+  adapter: PrismaAdapter(prisma),
 });
