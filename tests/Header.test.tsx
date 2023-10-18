fadf9268b5252592122d94d3dff198ad51ceaa45
Huynh Duc Dung <dunghd.it@gmail.com>
2023-10-18 20:40:42 +0800

    fix(test): mock fetch with vitest

--- tests/Header.test.tsx
+++ tests/Header.test.tsx
@@ -1,13 +1,21 @@
 import { render } from '@testing-library/react';
-import { describe, test } from 'vitest';
+import { describe, test, vi } from 'vitest';
 
 import Header from '../components/Header';
+import { SessionProvider } from 'next-auth/react';
+
+// @ts-expect-error Mock fetch with vitest
+global.fetch = vi.fn();
 
 /**
  * @jest-environment jsdom
  */
 describe('Header component', () => {
     test('should render header', () => {
-        render(<Header links={[]} />);
+        render(
+            <SessionProvider>
+                <Header links={[]} />
+            </SessionProvider>,
+        );
     });
 });
