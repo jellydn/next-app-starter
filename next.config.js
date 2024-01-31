69b430e3fb4bc897c275520c050474be566ea72e
Huynh Duc Dung <dunghd.it@gmail.com>
2024-01-31 20:54:38 +0800

    fix: use remote pattern for image

--- next.config.js
+++ next.config.js
@@ -1,12 +1,17 @@
-const { createSecureHeaders } = require("next-secure-headers");
+const { createSecureHeaders } = require('next-secure-headers');
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  async headers() {
-    return [{ source: "/(.*)", headers: createSecureHeaders() }];
-  },
-  images: {
-    dangerouslyAllowSVG: true,
-    domains: ["vercel.com"],
-  },
+    async headers() {
+        return [{ source: '/(.*)', headers: createSecureHeaders() }];
+    },
+    images: {
+        dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'vercel.com',
+            },
+        ],
+    },
 };
