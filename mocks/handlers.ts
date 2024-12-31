4f6bd1ca87db52d609362367fb100e167306b0db
Dung Huynh Duc <dunghd.it@gmail.com>
2024-12-31 21:48:27 +0800

    refactor(mocks): update MSW handlers to use new HttpResponse API

--- mocks/handlers.ts
+++ mocks/handlers.ts
@@ -1,33 +1,32 @@
-import { http } from 'msw';
+import { http, HttpResponse } from 'msw';
 
 const handlers = [
-    http.post('/login', async (_req, res, ctx) => {
+    http.post('/login', async () => {
         // Persist user's authentication in the session
         sessionStorage.setItem('is-authenticated', 'true');
-        return res(
-            // Respond with a 200 status code
-            ctx.status(200),
-        );
+
+        return new HttpResponse(null, { status: 200 });
     }),
-    http.get('/user', async (_req, res, ctx) => {
+    http.get('/user', async () => {
         // Check if the user is authenticated in this session
         const isAuthenticated = sessionStorage.getItem('is-authenticated');
         if (!isAuthenticated) {
             // If not authenticated, respond with a 403 error
-            return res(
-                ctx.status(403),
-                ctx.json({
+            return HttpResponse.json(
+                {
                     errorMessage: 'Not authorized',
-                }),
+                },
+                { status: 403 },
             );
         }
 
         // If authenticated, return a mocked user details
-        return res(
-            ctx.status(200),
-            ctx.json({
-                username: 'admin',
-            }),
+
+        return HttpResponse.json(
+            { username: 'admin' },
+            {
+                status: 200,
+            },
         );
     }),
 ];
